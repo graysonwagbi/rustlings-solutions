04f948c087c04d0b8a0b6cbaa3fbdf0f5b9a3778
grayson <88535111+graysonwagbi@users.noreply.github.com>
2024-01-17 19:55:45 +0800

    Update exercises 13

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -13,11 +11,20 @@ enum CreationError {
     Negative,
     Zero,
 }
+use crate::CreationError::*;
 
 impl PositiveNonzeroInteger {
+
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm... Why is this always returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value > 0{
+            Ok(PositiveNonzeroInteger(value as u64))
+        }else if( value == 0){
+            Err(Zero)
+        }else{
+            Err(Negative)
+        }
+        
     }
 }
 
